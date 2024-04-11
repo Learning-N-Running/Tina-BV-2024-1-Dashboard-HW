41600d6d82a5bcf6b07f8a4a143bfd927c10bade
Tina Lee <92625071+Learning-N-Running@users.noreply.github.com>
2024-04-11 20:41:01 +0900

    Feat: Reference hw1 answer

--- dashboard-client/src/components/atoms/dashboard/Date/index.tsx
+++ dashboard-client/src/components/atoms/dashboard/Date/index.tsx
@@ -1,34 +1,31 @@
 import s from './index.module.scss';
+import dayjs from 'dayjs';
+import 'dayjs/locale/ko';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
-export default function CustomDate() {
-  return <div className={s.date_container}>39초 전</div>;
-} // 함수 이름 변경
+dayjs.extend(relativeTime);
+dayjs.locale('ko', {
+  relativeTime: {
+    future: '%s 후',
+    past: '%s 전',
+    s: '%d초',
+    m: '1분',
+    mm: '%d분',
+    h: '1시간',
+    hh: '%d시간',
+    d: '1일',
+    dd: '%d일',
+    M: '1달',
+    MM: '%d달',
+    y: '1년',
+    yy: '%d년',
+  },
+});
 
-export function timeSince(transactionTime: string | Date): string {
-  // 함수 이름 변경
-  const transactionDate: Date = typeof transactionTime === 'string' ? new Date(transactionTime) : transactionTime;
-  const now: Date = new Date();
-  const secondsPast: number = (now.getTime() - transactionDate.getTime()) / 1000;
-
-  if (secondsPast < 60) {
-    // 1분 미만
-    return '방금 전';
-  } else if (secondsPast < 3600) {
-    // 1시간 미만
-    return `${Math.floor(secondsPast / 60)}분 전`;
-  } else if (secondsPast < 86400) {
-    // 24시간 미만
-    return `${Math.floor(secondsPast / 3600)}시간 전`;
-  } else if (secondsPast < 2592000) {
-    // 30일 미만
-    return `${Math.floor(secondsPast / 86400)}일 전`;
-  } else if (secondsPast < 31536000) {
-    // 1년 미만
-    return `${Math.floor(secondsPast / 2592000)}개월 전`;
-  } else {
-    return `${Math.floor(secondsPast / 31536000)}년 전`;
-  }
+export interface DateProps {
+  timestamp: number;
 }
 
-// 사용 예시
-console.log(timeSince('2023-02-10T15:00:00Z')); // 트랜잭션이 발생한 시간 예시
+export default function Date({ timestamp }: DateProps) {
+  return <div className={s.date_container}>{dayjs.unix(timestamp).fromNow()}</div>;
+}
--- dashboard-client/src/components/layouts/Header/index.tsx
+++ dashboard-client/src/components/layouts/Header/index.tsx
@@ -23,17 +23,20 @@ export default function Header() {
 
   const [isFetching, setIsFetching] = useState(false);
   const { wallet, connect, disconnect } = useContext(WalletContext);
+  const walletAddress = wallet?.accounts[0].address;
+  const chainId = wallet?.chains[0].id;
 
   /* 
     아래 함수는 서버로부터 가져온 사용자의 자산 정보를 전역 상태(Global state)에 저장하거나, 초기화하는 함수입니다. 
     구현 시 상황에 맞게 사용해 주세요.
+    SSR에서는 브라우저의 로컬스토리지 데이터에 접근할 수 없기 때문에 지워주는 절착 필요해서 clearUserInfo 함수를 해줘야하는 것.
   */
   const { updateUserInfo, clearUserInfo } = useUpdateUserInfo();
 
-  /* 
+  /*
     아래 코드는 추가하고자 하는 자산의 검증이 완료되었을 시, 서버로 추가하고자 하는 자산 정보를 보내는 코드예요.
     fetchUser 함수를 호출하여 사용자의 자산 및 트랜잭션 현황 정보를 업데이트하고 최신 정보를 내려받을 수 있어요. 
-    handleFetchUser 함수 내부에서만 호출되는 함수로, 과제 구현 시에는 handleFetchUser 함수를 사용해 주세요.
+    handleFetchUser 함수 내부에서만 호출되는 함수로, 과제 구현 시에는 handleFetchUser 함수를 사용해 주세요
   */
   const [fetchUser] = useFetchUser({
     onCompleted: () => {
@@ -45,9 +48,9 @@ export default function Header() {
     },
   });
 
-  /*
-    아래 함수는 지갑이 연결되었을 시, 사용자의 자산 및 트랜잭션 정보를 업데이트하며, 업데이트 상태를 관리합니다.
-  */
+  /**
+   * handleFetchUser : 지갑이 연결되었을 시, 사용자의 자산 및 트랜잭션 정보를 업데이트하며, 업데이트 상태를 관리합니다.
+   */
   const handleFetchUser = useCallback(
     async (address: string) => {
       setIsFetching(true);
@@ -64,17 +67,29 @@ export default function Header() {
       await disconnect({ label: wallet?.label });
       removeCookie(COOKIE_KEY.WALLET_ADDRESS, {});
       removeCookie(COOKIE_KEY.CHAIN_ID, {});
+      clearUserInfo();
     } else {
       await connect();
-      const walletAddress = wallet!?.accounts[0].address;
-      const chainId = wallet!?.chains[0].id;
       if (validateWalletNetwork(walletAddress, chainId)) {
-        setCookie(COOKIE_KEY.WALLET_ADDRESS, walletAddress, new Date(Date.now() + 1000 * 60 * 60 * 24), {});
-        setCookie(COOKIE_KEY.CHAIN_ID, chainId, new Date(Date.now() + 1000 * 60 * 60 * 24), {});
-        handleFetchUser(walletAddress);
+        setCookie(COOKIE_KEY.WALLET_ADDRESS, walletAddress!, new Date(Date.now() + 1000 * 60 * 60 * 24), {});
+        setCookie(COOKIE_KEY.CHAIN_ID, chainId!, new Date(Date.now() + 1000 * 60 * 60 * 24), {});
+        handleFetchUser(walletAddress!);
       }
     }
   };
+
+  useEffect(() => {
+    if (validateWalletNetwork(walletAddress, chainId)) {
+      if (
+        walletAddress !== getCookie(COOKIE_KEY.WALLET_ADDRESS, {}) ||
+        chainId !== getCookie(COOKIE_KEY.CHAIN_ID, {})
+      ) {
+        setCookie(COOKIE_KEY.WALLET_ADDRESS, walletAddress!, new Date(Date.now() + 1000 * 60 * 60 * 24), {});
+        setCookie(COOKIE_KEY.CHAIN_ID, chainId!, new Date(Date.now() + 1000 * 60 * 60 * 24), {});
+        handleFetchUser(walletAddress!);
+      }
+    }
+  }, [chainId, walletAddress, handleFetchUser]);
 
   return (
     <div className={s.header}>
